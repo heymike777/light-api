368bcf83d6f8cc0bdcff3610377eac7ef3264fb7
Mike Timashov <mike@cherrypiestudio.com>
2024-10-08 05:31:19 +0200

    Logs

--- src/routes/v1/Users.ts
+++ src/routes/v1/Users.ts
@@ -69,7 +69,7 @@ router.post(
       }
    
       const deviceId = req.body.deviceId;
-      console.log("LOGOUT", "userId", userId, "deviceId", deviceId);
+      console.log("LOGOUT", "userId", userId, "deviceId", deviceId, "body:", req.body, "query:", req.query, "params:", req.params);
 
       if (deviceId){
         await FirebaseManager.deletePushTokens(userId, deviceId);
