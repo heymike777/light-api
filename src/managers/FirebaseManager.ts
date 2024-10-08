0eba5a64617459b257b82f6ecd967884ca68fea0
Mike Timashov <mike@cherrypiestudio.com>
2024-10-08 09:07:25 +0200

    Update FirebaseManager.ts

--- src/managers/FirebaseManager.ts
+++ src/managers/FirebaseManager.ts
@@ -35,6 +35,8 @@ export class FirebaseManager {
             apns: {
                 payload: {
                   aps: {
+                    badge: 1,
+                    sound: 'default',
                     contentAvailable: true,
                   }
                 },
