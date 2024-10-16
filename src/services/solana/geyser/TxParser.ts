5fa53318bf49c4f1f9affb1fb49c0bed16974749
Mike Timashov <mike@cherrypiestudio.com>
2024-10-16 11:39:25 +0200

    Test logs

--- src/services/solana/geyser/TxParser.ts
+++ src/services/solana/geyser/TxParser.ts
@@ -145,6 +145,10 @@ export class TxParser {
         const heymikeAccount = accountKeys.find((account) => account.pubkey.toBase58() == '9Xt9Zj9HoAh13MpoB6hmY9UZz37L4Jabtyn8zE7AAsL');
         // console.log('!acc', 'signature:', signature, 'accountKeys:', accountKeys);
 
+        fs.appendFile('transactions_account_keys.txt', `${new Date()} ${signature} ${JSON.stringify(accountKeys)}\n`, (err) => {
+            if (err) console.error(err);
+        });
+
 
         const instructions = this.parseYellowstoneGrpcCompiledInstructions(confirmedTx.transaction?.message?.instructions, accountKeys, signature);
         const innerInstructions: web3.ParsedInnerInstruction[] = [];
@@ -189,21 +193,21 @@ export class TxParser {
             version: isVersioned ? 0 : 'legacy',
         };
 
-        const realParsedTxs = await SolanaManager.getParsedTransactions(newConnection(), [signature]);
+        // const realParsedTxs = await SolanaManager.getParsedTransactions(newConnection(), [signature]);
 
         if (heymikeAccount){ 
-            fs.appendFile('mike_txs.txt', `${new Date()} ${signature} parseGeyserTransactionWithMeta parsedTx: ${JSON.stringify(parsedTransactionWithMeta)} realParsedTx: ${JSON.stringify(realParsedTxs)}\n`, (err) => {
+            fs.appendFile('mike_txs.txt', `${new Date()} ${signature} parseGeyserTransactionWithMeta parsedTx: ${JSON.stringify(parsedTransactionWithMeta)}}\n`, (err) => {
                 if (err) console.error(err);
             });
 
-            console.log('!heymikeAccount parseGeyserTransactionWithMeta', 'signature:', signature);
-            try{
-                console.log("parseGeyserTransactionWithMeta", 'parsedTx', signature, JSON.stringify(parsedTransactionWithMeta));
-                console.log("parseGeyserTransactionWithMeta", 'realParsedTx', signature, JSON.stringify(realParsedTxs));    
-            }
-            catch (e){
-                console.error("parseGeyserTransactionWithMeta", 'error', signature, 'cannot stringify', e);
-            }
+            // console.log('!heymikeAccount parseGeyserTransactionWithMeta', 'signature:', signature);
+            // try{
+            //     console.log("parseGeyserTransactionWithMeta", 'parsedTx', signature, JSON.stringify(parsedTransactionWithMeta));
+            //     // console.log("parseGeyserTransactionWithMeta", 'realParsedTx', signature, JSON.stringify(realParsedTxs));    
+            // }
+            // catch (e){
+            //     console.error("parseGeyserTransactionWithMeta", 'error', signature, 'cannot stringify', e);
+            // }
         }
 
         return parsedTransactionWithMeta;
