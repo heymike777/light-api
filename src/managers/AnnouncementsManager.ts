686195d67d104041613b25530adf8643a0b3dccd
Mike Timashov <mike@cherrypiestudio.com>
2025-01-02 08:02:00 +0100

    Update AnnouncementsManager.ts

--- src/managers/AnnouncementsManager.ts
+++ src/managers/AnnouncementsManager.ts
@@ -7,18 +7,18 @@ export class AnnouncementsManager {
 
     static async getAnnouncements(): Promise<Announcement[]> {
         const announcements: Announcement[] = [
-            {
-                image: 'https://pbs.twimg.com/profile_banners/1457612818681896963/1671371929/1500x500',
-                url: 'https://light.app',
-            },
-            {
-                image: 'https://pbs.twimg.com/profile_banners/1644933348274446336/1728626232/1500x500',
-                url: 'https://light.app',
-            },
-            {
-                image: 'https://pbs.twimg.com/profile_banners/199225029/1666359577/1500x500',
-                url: 'https://light.app',
-            }
+            // {
+            //     image: 'https://pbs.twimg.com/profile_banners/1457612818681896963/1671371929/1500x500',
+            //     url: 'https://light.app',
+            // },
+            // {
+            //     image: 'https://pbs.twimg.com/profile_banners/1644933348274446336/1728626232/1500x500',
+            //     url: 'https://light.app',
+            // },
+            // {
+            //     image: 'https://pbs.twimg.com/profile_banners/199225029/1666359577/1500x500',
+            //     url: 'https://light.app',
+            // }
         ];
 
         return announcements
