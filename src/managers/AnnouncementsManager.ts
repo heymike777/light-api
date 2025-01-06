d871f0a8fcb36fe6c36ccee72d04cbf0d260507f
Mike Timashov <mike@cherrypiestudio.com>
2025-01-06 05:34:26 +0100

    Update AnnouncementsManager.ts

--- src/managers/AnnouncementsManager.ts
+++ src/managers/AnnouncementsManager.ts
@@ -8,17 +8,9 @@ export class AnnouncementsManager {
     static async getAnnouncements(): Promise<Announcement[]> {
         const announcements: Announcement[] = [
             // {
-            //     image: 'https://light.dangervalley.com/news/light_on_tg.png',
-            //     url: 'https://light.app',
+            //     image: 'https://light.dangervalley.com/news/join_community.png',
+            //     url: 'https://t.me/LightBotNews',
             // },
-            // {
-            //     image: 'https://pbs.twimg.com/profile_banners/1644933348274446336/1728626232/1500x500',
-            //     url: 'https://light.app',
-            // },
-            // {
-            //     image: 'https://pbs.twimg.com/profile_banners/199225029/1666359577/1500x500',
-            //     url: 'https://light.app',
-            // }
         ];
 
         return announcements
