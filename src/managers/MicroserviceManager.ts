d50b341dba37f7035ab03c224108b0e5be8a0472
Mike Timashov <mike@cherrypiestudio.com>
2025-02-12 22:17:43 +0100

    Update MicroserviceManager.ts

--- src/managers/MicroserviceManager.ts
+++ src/managers/MicroserviceManager.ts
@@ -4,14 +4,14 @@ import { LogManager } from "./LogManager";
 export const kServiceKey = 'KjeisSkasfsJK21-sd2lsdksjE3L-13LRKJ';
 
 export class MicroserviceManager {
-    static baseUrl = 'http://127.0.0.1:3340/api/v1/service';
+    // static baseUrl = 'http://127.0.0.1:3340/api/v1/service';
 
     static async geyserResubscribe(){
         // send POST API to /geyser/resubscribe with axios
         
         try {
             const { data } = await axios({
-                url: `${this.baseUrl}/geyser/resubscribe`,
+                url: `http://127.0.0.1:3340/api/v1/service/geyser/resubscribe`,
                 method: 'post',
                 headers: {
                     'Content-Type': 'application/json',
@@ -29,7 +29,7 @@ export class MicroserviceManager {
     static async receivedTx(geyserId: string, signature: string, txData: string){
         try {
             const { data } = await axios({
-                url: `${this.baseUrl}/main/received-tx`,
+                url: `http://127.0.0.1:3333/api/v1/service/main/received-tx`,
                 method: 'post',
                 headers: {
                     'Content-Type': 'application/json',
@@ -52,7 +52,7 @@ export class MicroserviceManager {
     static async sendMessageToTelegram(messageData: string){
         try {
             const { data } = await axios({
-                url: `${this.baseUrl}/telegram/send-message`,
+                url: `http://127.0.0.1:3342/api/v1/service/telegram/send-message`,
                 method: 'post',
                 headers: {
                     'Content-Type': 'application/json',
