20eca8bc304209fd23f7a9db3a897596b2ab7c09
Mike Timashov <mike@cherrypiestudio.com>
2025-02-14 12:04:27 +0100

    Update AnnouncementsManager.ts

--- src/managers/AnnouncementsManager.ts
+++ src/managers/AnnouncementsManager.ts
@@ -6,16 +6,25 @@ export interface Announcement {
 export class AnnouncementsManager {
 
     static async getAnnouncements(): Promise<Announcement[]> {
+        const maintenanceMode = false;
+
         const announcements: Announcement[] = [
             // {
             //     image: 'https://light.dangervalley.com/news/join_community.png',
             //     url: 'https://t.me/LightAppLounge',
             // },
-            {
+        ];
+
+        if (maintenanceMode){
+            announcements.push({
                 image: 'https://light.dangervalley.com/news/maintenance.png',
                 url: 'https://light.app/',
-            },
-        ];
+            });
+        }
+
+        //TODO: add announcement "Follow us on X" 
+        //TODO: add announcement "Rate the app on AppStore / Google Play"
+        //TODO: add announcement "New app version is available. Update in AppStore / Google Play"
 
         return announcements
     }
