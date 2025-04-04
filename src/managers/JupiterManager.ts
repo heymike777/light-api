f6954e60a5be856e0451588bc5c2572cf0d480e4
Mike Timashov <mike@cherrypiestudio.com>
2025-04-04 20:47:30 +0200

    Update JupiterManager.ts

--- src/managers/JupiterManager.ts
+++ src/managers/JupiterManager.ts
@@ -43,8 +43,9 @@ export class JupiterManager {
             const response = await axios.get(url);
             if (response.status === 200) {
                 const data = response.data?.data;
-                console.log('JupiterManager', 'getPrices', 'response:', response.data);
                 if (data) {
+                    console.log('JupiterManager', 'getPrices', 'data:', data, 'keys:', Object.keys(data));
+
                     return Object.keys(data).map(key => {
                         return {
                             address: key,
