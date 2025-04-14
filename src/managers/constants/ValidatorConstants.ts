da4190b97cad0c3dc504a2fb405b0e623f19cf6a
Mike Timashov <mike@cherrypiestudio.com>
2025-04-14 16:05:10 +0200

    Light Treasury wallet

--- src/managers/constants/ValidatorConstants.ts
+++ src/managers/constants/ValidatorConstants.ts
@@ -161,6 +161,7 @@ export const kKnownAddresses: {[key: string]: {name: string}} = {
     'DttWaMuVvTiduZRnguLF7jNxTgiMBZ1hyAumKUiL2KRL': {name: `Jitotip 7`},
     '3AVi9Tg9Uo68tJfuvoKvqKNWKkC5wPdSSdeBnizKZ6jT': {name: `Jitotip 8`},
     '43DbAvKxhXh1oSxkJSqGosNw3HpBnmsWiak6tB5wpecN': {name: `Backpack Exchange`},
+    'LFEEa8RzpbWaFgzPXLJQcGGDYLAeaaBAMp6WWCgxJM3': {name: `Light Treasury`}
 }
 
 export const kSolscanAddresses: {[key: string]: {name: string}} = {};
