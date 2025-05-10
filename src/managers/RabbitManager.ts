6221890d19ab8fab0d70437bacd832c652a34438
Mike Timashov <mike@cherrypiestudio.com>
2025-05-10 22:15:40 +0200

    RabbitMQ streams

--- src/managers/RabbitManager.ts
+++ src/managers/RabbitManager.ts
@@ -1,10 +1,12 @@
-import amqp, { Channel, ChannelModel, Connection, ConsumeMessage } from "amqplib";
+import amqp, { Channel, ChannelModel, ConsumeMessage } from "amqplib";
 import { SendMessageData } from "./bot/BotTypes";
 import { BotManager } from "./bot/BotManager";
+import { Client, Offset } from "rabbitmq-stream-js-client";
 
 export class RabbitManager {
 
-    static conn: ChannelModel | undefined = undefined;
+    // static conn: ChannelModel | undefined = undefined;
+    static client: Client | undefined = undefined;
     static cachedMessages: { [key: string]: Date } = {};
 
     static cleanCache() {
@@ -17,6 +19,53 @@ export class RabbitManager {
         }
     }
 
+    static async getRabbit(): Promise<Client | undefined> {
+        if (this.client) return this.client;
+        
+        this.client = await Client.connect({
+            hostname: "localhost",
+            port: 5552,
+            username: "guest",
+            password: "guest",
+            vhost: "/",
+        });
+
+        console.log("RabbitMQ: Making sure the stream exists...");
+        const streamSizeRetention = 5 * 1e9
+        await this.client.createStream({ stream: 'light-telegram', arguments: { "max-length-bytes": streamSizeRetention } });
+        
+        return this.client;
+    }
+
+    static async publishTelegramMessage(msg: SendMessageData) {
+        try {
+            console.log('Rabbit - publishTelegramMessage', msg);
+            const rabbitStreamClient = await this.getRabbit();
+            if (!rabbitStreamClient){
+                return;
+            }
+
+            const publisher = await rabbitStreamClient.declarePublisher({ stream: 'light-telegram' });
+            await publisher.send(Buffer.from(JSON.stringify(msg)));
+        }
+        catch (e){
+            console.error('Rabbit - publishTelegramMessage', 'error:', e);
+        }
+    }
+
+    static async listenToTelegramMessages() {
+        console.log("Rabbit - listening to telegram messages");
+        const rabbitStreamClient = await this.getRabbit();
+        if (!rabbitStreamClient){
+            return;
+        }
+        
+        await rabbitStreamClient.declareConsumer({ stream: 'light-telegram', offset: Offset.first() }, (message) => {
+            console.log(`RabbitMQ: Received message ${message.content.toString()}`)
+        })
+    }
+
+    /*
     static async getRabbit(): Promise<ChannelModel | undefined> {
         if (this.conn) return this.conn;
         if (!process.env.AMQP_URL) return undefined;
@@ -97,6 +146,7 @@ export class RabbitManager {
             }
         });
     }
+    */
 
     static async receivedMessage(payload: SendMessageData, msg?: ConsumeMessage, ch?: Channel){
         try {
