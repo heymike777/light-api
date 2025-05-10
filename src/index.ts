40a33970da9b1b88cde89152a8759b8761c56763
Mike Timashov <mike@cherrypiestudio.com>
2025-05-10 13:34:26 +0200

    upd

--- src/index.ts
+++ src/index.ts
@@ -110,7 +110,7 @@ const start = async () => {
     await connectToRedis();
 
     if (EnvManager.isTelegramProcess){
-        RabbitManager.listenToTelegramMessages();
+        // RabbitManager.listenToTelegramMessages();
     }
 
     const port = process.env.PORT;
