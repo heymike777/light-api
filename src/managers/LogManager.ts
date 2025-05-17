879f4d9c809384d1f729f3a517b028bd497bef30
Mike Timashov <mike@cherrypiestudio.com>
2025-05-17 14:04:01 +0200

    Write logs to file

--- src/managers/LogManager.ts
+++ src/managers/LogManager.ts
@@ -1,8 +1,25 @@
+import { createLogger, format, transports } from 'winston';
+
 export class LogManager {
     static isLogsEnabled = false;
     static isErrorsEnabled = true;
 
+    static logger = createLogger({
+        level: 'info',
+        format: format.combine(
+            format.timestamp(),
+            format.printf(({ timestamp, level, message }) =>
+                `${timestamp} [${level.toUpperCase()}, ${process.env.SERVER_NAME}]: ${message}`
+            )
+        ),
+        transports: [
+            new transports.File({ filename: 'logs/error.log', level: 'error' }),
+            new transports.File({ filename: 'logs/combined.log' }),
+        ],
+    });
+
     static log(...args: any[]){
+        this.logger.info(args.map((a: any) => a.toString()).join(' '));
         if (this.isLogsEnabled){
             try {
                 console.log(new Date(), ...args);
@@ -13,6 +30,7 @@ export class LogManager {
     }
 
     static forceLog(...args: any[]){
+        this.logger.info(args.map((a: any) => a.toString()).join(' '));
         try {
             console.log(new Date(), ...args);
         }
@@ -21,6 +39,7 @@ export class LogManager {
     }
 
     static error(...args: any[]){
+        this.logger.error(args.map((a: any) => a.toString()).join(' '));
         if (this.isErrorsEnabled){
             try {
                 console.error(new Date(), ...args);
